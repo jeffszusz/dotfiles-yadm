49c810a75a292abc061a1fe25b6fdeccf11d7050
Jeffrey Szusz <jeff.szusz@gmail.com>
2026-03-29 21:48:33 -0400

    set up seperate lazyvim plugin configurations for home and work - I don't use coffee at home, I don't use svelte at work, etc

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,15 +1,18 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 
--- Force Neovim to use the OSC 52 clipboard provider instead of pbcopy
-vim.g.clipboard = {
-  name = "OSC 52",
-  copy = {
-    ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
-    ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
-  },
-  paste = {
-    ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
-    ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
-  },
-}
+-- When running remotely over SSH, use OSC 52 to access the local clipboard.
+-- Locally, nvim's built-in pbcopy/pbpaste support is used instead.
+if vim.env.SSH_TTY or vim.env.SSH_CLIENT then
+  vim.g.clipboard = {
+    name = "OSC 52",
+    copy = {
+      ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+      ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+    },
+    paste = {
+      ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
+      ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
+    },
+  }
+end
